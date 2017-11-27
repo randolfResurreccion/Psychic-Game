14d649ff4568f0dc3156ff9f05f33ea17b5d8628
RANDOLF RESURRECCION <randolfresurreccion@RANDOLFs-MacBook-Pro.local>
2017-11-27 15:09:01 -0800

    added to lowercase and validation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,40 +15,54 @@ var guessedLetters = [];
 
 document.onkeyup = function (event) {
 
-    userGuess = event.key;
-    guessedLetters.push(userGuess);
 
-    mysteryLetter = letters[Math.floor(Math.random() * letters.length)];
-    // console.log(mysteryLetter);
+    // validate if user enters a letter.
+    // use conditionals if user didnt enter a letter retun.
 
-    if (userGuess === mysteryLetter) {
-        wins++;
-        reset();
+    userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+    if (letters.indexOf(userGuess) < 0) {
+        alert("Please enter a letter");
+    } else {
+        guessedLetters.push(userGuess);
+
+        mysteryLetter = letters[Math.floor(Math.random() * letters.length)];
+        // console.log(mysteryLetter);
+
+        if (userGuess === mysteryLetter) {
+            wins++;
+            reset();
+        }
+        else {
+            guessesLeft--;
+        }
+
+        if (guessesLeft === 0) {
+            loses++;
+            reset();
+
+        }
+
+        var html = "<h1> The Psychic Game</h1>" +
+            "<p>Guess what letter i'm thinking of: </p>" +
+            "<p>Wins: " + wins + "</p>" +
+            "<p>Losses: " + loses + "</p>" +
+            "<p>Guesses Left: " + guessesLeft + "</p>" +
+            "<p>Your Guesses so far: " + guessedLetters + "</p>";
+
+        document.querySelector("#game").innerHTML = html;
     }
-    else {
-        guessesLeft--;
-    }
+    // console.log(userGuess);
 
-    if (guessesLeft === 0) {
-        loses++;
-        reset();
 
-    }
 
-    var html = "<h1> The Psychic Game</h>" +
-        "<p>Guess what letter i'm thinking of: </p>" +
-        "<p>Wins: " + wins + "</p>" +
-        "<p>Losses: " + loses + "</p>" +
-        "<p>Guesses Left: " + guessesLeft + "</p>" +
-        "<p>Your Guesses so far: " + guessedLetters + "</p>";
 
-    document.querySelector("#game").innerHTML = html;
 }
 
 function reset() {
     guessesLeft = 9;
     guessedLetters = [];
     mysteryLetter = letters[Math.floor(Math.random() * letters.length)];
-    
+
 }
 
